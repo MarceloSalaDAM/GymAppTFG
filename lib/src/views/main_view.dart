696411822a9ee2dcf161a69e17d24d14cbf0f605
MarceloSalaDAM <dam.marcelo.dosala@gmail.com>
2023-10-15 19:55:03 +0200

    Desarrollo ventana main

--- lib/src/views/main_view.dart
+++ lib/src/views/main_view.dart
@@ -1,17 +1,25 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../custom/InputText.dart';
-
-class MainViewApp extends StatelessWidget {
-  //Vista para el logeo a la aplicacion
+class MainViewApp extends StatefulWidget {
   MainViewApp({Key? key}) : super(key: key);
 
+  @override
+  _MainViewAppState createState() => _MainViewAppState();
+}
+
+class _MainViewAppState extends State<MainViewApp> {
+  int _currentIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Página Básica'),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -44,6 +52,74 @@ class MainViewApp extends StatelessWidget {
               child: Text('Mostrar Diálogo'),
             ),
           ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Acción al presionar el botón central
+          print('Botón central presionado');
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.black,
+        // Color de fondo personalizado
+        elevation: 1.0, // Controla la sombra del botón
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // Posición central
+      bottomNavigationBar: Container(
+        height: 55,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.green],
+            // Cambia estos colores según tus preferencias
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+        ),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          color: Colors.transparent,
+          // Establece el color de la barra de navegación como transparente
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _onItemTapped(0);
+                  });
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.query_stats_outlined,
+                      color: _currentIndex == 0 ? Colors.white : Colors.grey,
+                      size: 38,
+                    ),
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _onItemTapped(1);
+                  });
+                },
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Icon(
+                      Icons.person_outline_outlined,
+                      color: _currentIndex == 1 ? Colors.white : Colors.grey,
+                      size: 38,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
