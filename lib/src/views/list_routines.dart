6ea1acbd3cf0368c13661d27ef2650c03aa2aa14
MarceloSalaDAM <dam.marcelo.dosala@gmail.com>
2023-12-11 19:09:56 +0100

    PERSONALIZACION VISTA LISTA RUTINAS

--- lib/src/views/list_routines.dart
+++ lib/src/views/list_routines.dart
@@ -62,13 +62,34 @@ class _RutinasUsuarioViewState extends State<RutinasUsuarioView> {
 
   Widget _buildRutinaTile(Rutina rutina) {
     return Card(
-      elevation: 2.0,
-      margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      color: Colors.blueGrey,
+      shape: RoundedRectangleBorder(
+        side: const BorderSide(),
+      ),
+      elevation: 4.0,
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
       child: ExpansionTile(
-        title: Text(rutina.nombreRutina),
+        title: Text(
+          rutina.nombreRutina,
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.w900, color: Colors.white),
+        ),
         children: [
           ListTile(
-            title: Text('Descripción: ${rutina.descripcionRutina}'),
+            title: Text(
+              'Descripción:',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            subtitle: Text(
+              '${rutina.descripcionRutina}',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black),
+            ),
           ),
           _buildDiasList(rutina.dias),
         ],
@@ -78,37 +99,83 @@ class _RutinasUsuarioViewState extends State<RutinasUsuarioView> {
 
   Widget _buildDiasList(Map<String, dynamic> dias) {
     List<Widget> tiles = [];
+    final eyeIcon = Icon(
+        Icons.remove_red_eye); // Icono genérico para todas las ExpansionTile
+
     dias.forEach((nombreDia, infoDia) {
       List<Widget> ejerciciosTiles = [];
 
-      if (infoDia['ejercicios'] != null) {
-        for (var ejercicio in infoDia['ejercicios']) {
-          ejerciciosTiles.add(
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Nombre: ${ejercicio['nombre']}'),
-                Text('Repeticiones: ${ejercicio['repeticiones']}'),
-                Text('Series: ${ejercicio['series']}'),
-                // Agrega más detalles según sea necesario
-              ],
-            ),
-          );
-        }
+      /*if (infoDia['ejercicios'] != null) {
+      for (var ejercicio in infoDia['ejercicios']) {
+        ejerciciosTiles.add(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Nombre: ${ejercicio['nombre']}'),
+              Text('Repeticiones: ${ejercicio['repeticiones']}'),
+              Text('Series: ${ejercicio['series']}'),
+              // Agrega más detalles según sea necesario
+            ],
+          ),
+        );
       }
+    }*/
 
-      tiles.add(ListTile(
-        title: Text(nombreDia),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: ejerciciosTiles,
+      tiles.add(Container(
+        margin: EdgeInsets.fromLTRB(25, 0, 25, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Text(
+                nombreDia,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                  decoration: TextDecoration.underline,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 5),
+            if (infoDia['grupo'] != null)
+              Text(
+                '${infoDia['grupo']}',
+                style: TextStyle(fontWeight: FontWeight.w700,fontSize: 18),
+              ),
+            const SizedBox(height: 5),
+            // Puedes agregar más detalles sobre el día aquí
+            ...ejerciciosTiles,
+          ],
         ),
       ));
     });
 
-    return Column(
-      children: tiles,
+    return Container(
+      margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.black,
+          width: 2,
+          style: BorderStyle.solid,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(18.0)),
+      ),
+      child: Column(
+        children: [
+          ...tiles,
+          IconButton(
+            icon: eyeIcon,
+            onPressed: () {
+              // Lógica que se ejecutará al hacer clic en el botón de ojo
+              // Puedes abrir un nuevo widget o realizar alguna otra acción
+            },
+          ),
+        ],
+      ),
     );
   }
-
 }
