26494b7adb6e04daaae08e82890f016b8115ec6d
MarceloSalaDAM <dam.marcelo.dosala@gmail.com>
2024-04-03 19:18:31 +0200

    arreglos edit rutina

--- lib/src/create_views/edit_routine.dart
+++ lib/src/create_views/edit_routine.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,6 +9,7 @@ class EjerciciosDiaView extends StatefulWidget {
   final String dia;
   final Map<String, dynamic> ejerciciosDia;
   final String rutinaId; // Nuevo parámetro
+  final StreamController<void> _streamController = StreamController<void>();
 
   EjerciciosDiaView(
       {required this.dia, required this.ejerciciosDia, required this.rutinaId});
@@ -21,15 +24,15 @@ class _EjerciciosDiaViewState extends State<EjerciciosDiaView> {
   Map<int, TextEditingController> _seriesControllers = {};
   Map<int, TextEditingController> _repeticionesControllers = {};
   Set<int> _ejerciciosEliminados =
-      {}; // Variable para almacenar índices de ejercicios eliminados
+  {}; // Variable para almacenar índices de ejercicios eliminados
 
   @override
   void initState() {
     super.initState();
     // Inicializa los controladores con los valores reales de los ejercicios
     for (int index = 0;
-        index < (widget.ejerciciosDia['ejercicios'] ?? []).length;
-        index++) {
+    index < (widget.ejerciciosDia['ejercicios'] ?? []).length;
+    index++) {
       final peso = widget.ejerciciosDia['ejercicios'][index]['peso'] ?? '';
       final series = widget.ejerciciosDia['ejercicios'][index]['series'] ?? '';
       final repeticiones =
@@ -69,14 +72,14 @@ class _EjerciciosDiaViewState extends State<EjerciciosDiaView> {
               [widget.ejerciciosDia['ejercicios'][index]])
         });
         setState(() {
+          // Solo eliminamos el ejercicio correspondiente al índice proporcionado
           widget.ejerciciosDia['ejercicios'].removeAt(index);
-          _ejerciciosEliminados
-              .add(index); // Agrega el índice del ejercicio eliminado
         });
         print('Ejercicio eliminado exitosamente de la rutina del usuario.');
       } else {
         print('Error: Usuario no logueado.');
       }
+      widget._streamController.add(null);
     } catch (error) {
       print('Error al eliminar el ejercicio de la rutina del usuario: $error');
     }
@@ -89,98 +92,87 @@ class _EjerciciosDiaViewState extends State<EjerciciosDiaView> {
       appBar: AppBar(
         title: Text('${widget.dia} Ejercicios'),
       ),
-      body: ListView.builder(
-        itemCount: (widget.ejerciciosDia['ejercicios'] ?? []).length,
-        itemBuilder: (context, index) {
-          final isExpanded = _expandedState[index] ?? false;
+      body: StreamBuilder<void>(
+        stream: widget._streamController.stream,
+        builder: (context, snapshot) {
+          return ListView.builder(
+            itemCount: (widget.ejerciciosDia['ejercicios'] ?? []).length,
+            itemBuilder: (context, index) {
+              final isExpanded = _expandedState[index] ?? false;
 
-          // Verifica si el índice está en la lista de ejercicios eliminados
-          if (_ejerciciosEliminados.contains(index)) {
-            return SizedBox
-                .shrink(); // Devuelve un widget vacío si el ejercicio fue eliminado
-          }
+              // Verifica si el índice está en la lista de ejercicios eliminados
+              if (_ejerciciosEliminados.contains(index)) {
+                return SizedBox
+                    .shrink(); // Devuelve un widget vacío si el ejercicio fue eliminado
+              }
 
-          return Dismissible(
-            key: Key(index.toString()),
-            background: Container(
-              color: Colors.red,
-              alignment: Alignment.centerRight,
-              padding: EdgeInsets.only(right: 20.0),
-              child: Icon(Icons.delete, color: Colors.white),
-            ),
-            onDismissed: (direction) {
-              setState(() {
-                // Remove the dismissed item from the list
-                widget.ejerciciosDia['ejercicios'].removeAt(index);
-                // Also remove the corresponding TextEditingController
-                _pesoControllers.remove(index);
-                _seriesControllers.remove(index);
-                _repeticionesControllers.remove(index);
-              });
-              // Call _eliminarEjercicio after updating the state to ensure consistency
-              _eliminarEjercicio(widget.rutinaId, index);
-            },
-            direction: isExpanded
-                ? DismissDirection.none
-                : DismissDirection.endToStart,
-            child: Card(
-              child: ExpansionTile(
-                onExpansionChanged: (expanded) {
-                  setState(() {
-                    _expandedState[index] = expanded;
-                  });
-                },
-                title: Text(
-                  widget.ejerciciosDia['ejercicios'][index]['nombre'],
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+              return Card(
+                child: ExpansionTile(
+                  onExpansionChanged: (expanded) {
+                    setState(() {
+                      _expandedState[index] = expanded;
+                    });
+                  },
+                  title: Text(
+                    widget.ejerciciosDia['ejercicios'][index]['nombre'],
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  initiallyExpanded: isExpanded,
+                  children: isExpanded
+                      ? [
+                    ListTile(
+                      title: TextField(
+                        decoration:
+                        InputDecoration(labelText: 'Peso (kg)'),
+                        controller: _pesoControllers[index],
+                        onChanged: (value) {
+                          // Manejar los cambios en el campo de peso aquí
+                        },
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                      ),
+                    ),
+                    ListTile(
+                      title: TextField(
+                        decoration:
+                        InputDecoration(labelText: 'Series'),
+                        controller: _seriesControllers[index],
+                        onChanged: (value) {
+                          // Manejar los cambios en el campo de series aquí
+                        },
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                      ),
+                    ),
+                    ListTile(
+                      title: TextField(
+                        decoration: InputDecoration(
+                            labelText: 'Repeticiones'),
+                        controller: _repeticionesControllers[index],
+                        onChanged: (value) {
+                          // Manejar los cambios en el campo de repeticiones aquí
+                        },
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                      ),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        _eliminarEjercicio(widget.rutinaId, index);
+                      },
+                    ),
+                  ]
+                      : [],
                 ),
-                initiallyExpanded: isExpanded,
-                children: isExpanded
-                    ? [
-                        ListTile(
-                          title: TextField(
-                            decoration: InputDecoration(labelText: 'Peso (kg)'),
-                            controller: _pesoControllers[index],
-                            onChanged: (value) {
-                              // Manejar los cambios en el campo de peso aquí
-                            },
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                          ),
-                        ),
-                        ListTile(
-                          title: TextField(
-                            decoration: InputDecoration(labelText: 'Series'),
-                            controller: _seriesControllers[index],
-                            onChanged: (value) {
-                              // Manejar los cambios en el campo de series aquí
-                            },
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                          ),
-                        ),
-                        ListTile(
-                          title: TextField(
-                            decoration:
-                                InputDecoration(labelText: 'Repeticiones'),
-                            controller: _repeticionesControllers[index],
-                            onChanged: (value) {
-                              // Manejar los cambios en el campo de repeticiones aquí
-                            },
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                          ),
-                        ),
-                      ]
-                    : [],
-              ),
-            ),
+              );
+            },
           );
         },
       ),
